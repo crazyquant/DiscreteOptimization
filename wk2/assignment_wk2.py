344fbab1a752c51e005162f748e7d1fa8c10a7a3
Benjamin Cheng <benjamincheng@hotmail.com>
2021-01-17 16:33:59 +1100

    knapsack assign2

--- wk2/assignment_wk2.py
+++ wk2/assignment_wk2.py
@@ -1,19 +1,28 @@
 import numpy
 import os
+import sys
+
+
+def testing(l1, l2):
+    outputData = str(19) + ' ' + str(0) + '\n'
+    taken = [0, 0, 1, 1]
+    outputData += ' '.join(map(str, taken))
+    return outputData
 
 
 def solveIt(inputData):
     lines = inputData.split('\n')
-    a, b = map(list, zip(*(s.split(" ") for s in lines)))
-    print(a)
-    print(b)
-    return lines
+    l1, l2 = map(list, zip(*(s.split(" ") for s in lines)))
+    return testing(l1, l2)
+
+if(len(sys.argv) == 2):
+    filename = sys.argv[1]
+else:
+    sys.exit("Error: No input file provided. Please enter the path\\filename of the input file as an argument.")
+
+f = open(filename, "r")
+print(solveIt(f.read()))
+f.close()
 
 
-inputFileName = "inputData1.dat"
-inputFileName = "datafile/" + inputFileName
-
-f = open(inputFileName, "r")
-
-solveIt(f.read())
 
